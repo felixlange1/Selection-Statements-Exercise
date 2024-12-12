30004106bc924a0dbf175073d9e32298d630fcfe
Felix Lange <felixlange1@gmail.com>
2024-12-11 22:56:06 -0800

    did if else statement exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 3;
+            
+            Console.WriteLine("Guess the number:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low.");
+            }
+            
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high.");
+            }
+
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+            
         }
     }
 }
